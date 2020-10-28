5ef9bb8422fc03d327d78a62d17f6ec9f1398e67
Estif017 <estifoteklearegay@gmail.com>
2020-10-27 20:08:22 -0700

    creating navbar aboutpage and home page on react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import React,{Fragment} from 'react'
+import {Switch, Route}from 'react-router-dom'
+
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+import About from './Pages/About/About';
+import Homepage from './Pages/Home/Homepage';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Fragment>
+     <NavBar/>
+     <div className="container">
+       <Switch>
+         <Route exact path='/'><Homepage/></Route>
+         <Route path='/about'><About/></Route>
+       </Switch>
+     </div>
+    </Fragment>
   );
 }
 
---
+++ client/src/Components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = ({icon,title}) => {
+    return (
+        <div className='navbar bg-primary'>
+            <Link to='/'>
+                <h1><i className={icon}/>{title}</h1>
+            </Link>
+            <nav>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+NavBar.defaultProps = {
+    title:'Contact Keeper',
+    icon: 'fas fa-id-card-alt'
+}
+
+export default NavBar
